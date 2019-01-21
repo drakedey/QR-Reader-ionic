34e956fb22b88f68a3222b7e036d46b7ec9b14a8
Joskar Hernandez <joskarandres97@gmail.com>
2019-01-21 08:25:20 -0400

    feature: tabs root defined

---
+++ src/pages/pages.index.ts
@@ -0,0 +1,4 @@
+export { HomePage } from './home/home';
+export { MapPage } from "./map/map";
+export { SavedPage } from "./saved/saved";
+export { TabsPage } from "./tabs/tabs";
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage, SavedPage } from "../pages.index";
+
+@IonicPage()
+@Component({
+  template:`
+    <ion-tabs>
+      <ion-tab tabIcon="home" tabTitle="Home" [root]="homePage"></ion-tab>
+      <ion-tab tabIcon="bookmark" tabTitle="History" [root]="savedPage"></ion-tab>
+    </ion-tabs>
+  `
+})
+export class TabsPage {
+
+  homePage: any;
+  savedPage: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.seTabPages();
+  }
+
+  seTabPages() {
+    this.homePage = HomePage;
+    this.savedPage = SavedPage;
+  }
+
+}
